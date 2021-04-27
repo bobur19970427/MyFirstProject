7b6beed442c038c3b597ee2af96fcf01b4ff7ce3
Bobur <77155984+bobur19970427@users.noreply.github.com>
2021-04-27 13:56:18 +0500

    Add words count

--- vievname/view.py
+++ vievname/view.py
@@ -5,5 +5,9 @@ def reverse(request):
 
 def reversed(request):
 	user_text = request.GET['usertext']
+	count_text = len(user_text.split(" "))
 	rever_text = user_text[::-1]
-	return render(request, 'reversed.html', {'usertext': user_text, 'revertext':rever_text})
+	return render(request, 'reversed.html', 
+		{'usertext': user_text, 
+		'revertext':rever_text, 
+		'counttext':count_text})
